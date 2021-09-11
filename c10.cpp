a7345bcff9c3c36e37f5eb1e7d99875700781e69
Aritra Samanta <asamanta2508@gmail.com>
2021-09-11 00:20:08 -0700

    Challenge 10 - Trial 1 - doesn't work.

--- c10.cpp
+++ c10.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
 #include "common.h"
+#include "crypto_func.h"
 
-#define KEY "YELLOW SUBMARINE"
+#define AES_BLOCK_SIZE_BYTES	16
 
 int main(int argc, const char * argv[])
 {
@@ -18,6 +19,31 @@ int main(int argc, const char * argv[])
 
     // Convert hex string to ASCII string
     _hex_to_ascii(hex_string, ascii_string);
+
+    // Create an IV of Zeroes
+    unsigned char * iv = new unsigned char[AES_BLOCK_SIZE_BYTES];
+    for (int i = 0; i < AES_BLOCK_SIZE_BYTES; i++)
+    {
+    	iv[i] = 0x0;
+    }
+
+    int plaintext_len = 0;
+    unsigned char * pt = new unsigned char[AES_BLOCK_SIZE_BYTES];
+    unsigned char * key = new unsigned char[AES_BLOCK_SIZE_BYTES];
+    unsigned char * mem = new unsigned char[AES_BLOCK_SIZE_BYTES];
+
+    memcpy(key, "YELLOW SUBMARINE", AES_BLOCK_SIZE_BYTES);
+
+    for (int i = 0; i < AES_BLOCK_SIZE_BYTES; i++)
+    {
+    	mem[i] = ascii_string[i];
+    }
+
+    plaintext_len = ecb_decrypt(mem, strlen((char *)mem), key, NULL, pt);
+
+    delete[] iv;
+    delete[] mem;
+    delete[] pt;
 
 	return 0;
 }
